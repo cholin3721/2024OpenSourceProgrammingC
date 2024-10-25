0e37c6eb6c2c16474d6d85a83c030e808cdff517
cholin3721 <cholin3721@gmail.com>
2024-10-25 15:37:02 +0900

    v2.0 Loop Sprintf

--- week09/main.go
+++ week09/main.go
@@ -1,57 +1,19 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
 	"math/rand"
-	"os"
-	"strconv"
-	"strings"
-	"time"
 )
 
 func main() {
+	//fmt.Printf("%f 나누기 %f은(는) %f입니다.\n", 1.0, 3.0, 1.0/3.0)  //console 출력
+	result := fmt.Sprintf("%0.2f 나누기 %0.2f은(는) %0.2f입니다.\n", 1.0, 3.0, 1.0/3.0) // 서식을 문자열로 리턴
+	fmt.Print(result)
 
-	rand.Seed(time.Now().Unix()) //seed 값이 고정되면 난수가 고정됨, 유닉스 시간을 가져와서 시드값 고정 못하게 함
-	answer := rand.Intn(6) + 1
-	var win bool = false
-
-	for guesses := 0; guesses < 3; guesses++ {
-		fmt.Printf("%d번의 기회가 남았습니다. 숫자 입력 : ", 3-guesses)
-		in := bufio.NewReader(os.Stdin)
-		i, err := in.ReadString('\n')
-		//fmt.Println(i, err)
-		if err != nil {
-			log.Fatal(err)
-		}
-		i = strings.TrimSpace(i)      //줄바꿈등 제거, 파이썬의 strip 함수와 비슷
-		guess, err := strconv.Atoi(i) //16진 정수형(32bit)으로 변환
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if answer == guess {
-			fmt.Println()
-			fmt.Println("입력하신 수는 정답입니다. 선물은 없습니다.")
-			fmt.Println()
-			win = true
-			break
-		} else if answer > guess {
-			fmt.Println()
-			fmt.Println("입력하신 수가 정답보다 작은 수입니다. LOW")
-			fmt.Println()
-		} else {
-			fmt.Println()
-			fmt.Println("입력하신 수는 정답보다 큰 수입니다. HIGH")
-			fmt.Println()
-		}
-
-	}
-
-	if win == true {
-		fmt.Println("You Win!!")
-	} else {
-		fmt.Printf("답은 %d임ㅋ 병신ㅋ\n", answer)
+	i := 1
+	for i <= 10 {
+		//fmt.Printf("%2d\n", i)
+		fmt.Printf("%4d\n", rand.Intn(1000)+1)
+		i++
 	}
 }
