dbe92a43d3ef0c8480ae2f81a76d77db827eb3e8
cholin3721 <cholin3721@gmail.com>
2024-11-15 15:22:25 +0900

    v3.9 array

--- week12/main.go
+++ week12/main.go
@@ -1,44 +1,22 @@
+// average calculates the average of several numbers.
 package main
 
 import (
 	"fmt"
-	"time"
+	"log"
+
+	"github.com/headfirstgo/datafile"
 )
 
 func main() {
-	var scores [3]int
-	scores[1] = 90
-	//fmt.Println(scores[1], scores[0]) //, scores[3])
-	//fmt.Printf("%#v\n", scores)
-	//fmt.Println(scores)
-
-	for i := 0; i < len(scores); i++ { //unsafe
-		fmt.Printf("%d ", scores[i])
+	numbers, err := datafile.GetFloats("data.txt")
+	if err != nil {
+		log.Fatal(err)
 	}
-
-	//리터럴은 값 그자체를 의미함
-	//var dates [3]time.Time
-	//dates[1] = time.Unix(1947200001, 0)
-	//fmt.Println(dates[1])
-
-	//dates := [3]time.Time{time.Unix(0, 0), time.Unix(1, 0), time.Unix(1947200001, 0)}
-	//fmt.Println(dates[0], dates[1], dates[2])
-
-	dates := [3]time.Time{
-		time.Unix(0, 0),
-		time.Unix(1, 0),
-		time.Unix(1947200001, 0), //need comma, 대괄호를 붙이면 가능하긴함
+	var sum float64 = 0
+	for _, number := range numbers {
+		sum += number
 	}
-	//fmt.Printf("%#v\n", dates) \n<<rune 값 : 10
-	//fmt.Println(dates)
-	//fmt.Println(dates[0], dates[1], dates[2])
-	//fmt.Println()
-	//for i, date := range dates {
-	//	fmt.Println(i, date)
-	//}
-	fmt.Println()
-	for _, date := range dates {
-		fmt.Println(date)
-	}
-
+	sampleCount := float64(len(numbers))
+	fmt.Printf("Average: %0.2f\n", sum/sampleCount)
 }
