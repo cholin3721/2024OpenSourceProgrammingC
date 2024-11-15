acdb2dbfd2210f672fd8dc3fdf45f45f16ba5857
cholin3721 <cholin3721@gmail.com>
2024-11-15 14:47:32 +0900

    v3.8 array

--- week12/main.go
+++ week12/main.go
@@ -8,9 +8,13 @@ import (
 func main() {
 	var scores [3]int
 	scores[1] = 90
-	fmt.Println(scores[1], scores[0]) //, scores[3])
-	fmt.Printf("%#v\n", scores)
-	fmt.Println(scores)
+	//fmt.Println(scores[1], scores[0]) //, scores[3])
+	//fmt.Printf("%#v\n", scores)
+	//fmt.Println(scores)
+
+	for i := 0; i < len(scores); i++ { //unsafe
+		fmt.Printf("%d ", scores[i])
+	}
 
 	//리터럴은 값 그자체를 의미함
 	//var dates [3]time.Time
@@ -25,7 +29,16 @@ func main() {
 		time.Unix(1, 0),
 		time.Unix(1947200001, 0), //need comma, 대괄호를 붙이면 가능하긴함
 	}
-	fmt.Printf("%#v\n", dates)
-	fmt.Println(dates)
-	fmt.Println(dates[0], dates[1], dates[2])
+	//fmt.Printf("%#v\n", dates) \n<<rune 값 : 10
+	//fmt.Println(dates)
+	//fmt.Println(dates[0], dates[1], dates[2])
+	//fmt.Println()
+	//for i, date := range dates {
+	//	fmt.Println(i, date)
+	//}
+	fmt.Println()
+	for _, date := range dates {
+		fmt.Println(date)
+	}
+
 }
